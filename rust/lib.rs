7cb8cd7e0fb34e06e1981e846e117e7787b2033b
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-08 08:53:16 -0700

    define as attribute

--- rust/lib.rs
+++ rust/lib.rs
@@ -4,8 +4,8 @@ extern crate proc_macro;
 use core::str::FromStr;
 use proc_macro::TokenStream;
 
-#[proc_macro]
-pub fn fam(_input: TokenStream) -> TokenStream {
+#[proc_macro_attribute]
+pub fn fam(_input: TokenStream, _attrs: TokenStream) -> TokenStream {
     let stream = TokenStream::from_str("#![no_std]");
     stream.expect("parse token stream")
 }
